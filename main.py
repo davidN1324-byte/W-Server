60e861af0ac8fa3de7a503feaee4e7a773f1fe45
david <you@example.com>
2025-03-21 03:23:14 +0200

    Logging

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from routes import router
 from utils import check_permissions
 from config import UPLOAD_FOLDER
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
