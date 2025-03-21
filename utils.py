fb4e360d09053f3fc771074f60f8176d0b6a7d39
david <you@example.com>
2025-03-21 02:52:26 +0200

    def get_mime_type Determining the MIME type of a file

--- utils.py
+++ utils.py
@@ -19,3 +19,6 @@ def get_unique_filename(file_name: str) -> str:
     file_hash = hashlib.sha256(file_name.encode()).hexdigest()
     return file_hash + Path(file_name).suffix
 
+def get_mime_type(file_bytes: bytes) -> str:
+    mime = magic.Magic(mime=True)
+    return mime.from_buffer(file_bytes)
