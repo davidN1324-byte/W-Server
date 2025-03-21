e40419bf4d7617b47a96030b805e9a6cd39280dd
david <you@example.com>
2025-03-21 04:20:09 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 import logging
 from fastapi.staticfiles import StaticFiles
 from app.routes import router
-from utils import check_permissions
-from config import UPLOAD_FOLDER
+from app.utils import check_permissions
+from app.config import UPLOAD_FOLDER
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
