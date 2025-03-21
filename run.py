d40a71cab0a50530386cdf06dc89125bcd41e08d
david <you@example.com>
2025-03-21 03:29:50 +0200

    settings for launch

--- run.py
+++ run.py
@@ -2,3 +2,12 @@ import os
 import uvicorn
 from app.main import app
 
+if __name__ == "__main__":
+    ssl_certfile = os.getenv("SSL_CERTFILE", "Cert/cert.pem")
+    ssl_keyfile = os.getenv("SSL_KEYFILE", "Cert/key.pem")
+
+    uvicorn.run(app, 
+                host="0.0.0.0", 
+                port=5000, 
+                ssl_certfile=ssl_certfile, 
+                ssl_keyfile=ssl_keyfile)
