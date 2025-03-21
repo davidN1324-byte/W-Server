9a9fb7a3bb13fc77dfbec5e34f37bbfea99e79f1
david <you@example.com>
2025-03-21 04:19:29 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import logging
 from fastapi.staticfiles import StaticFiles
-from routes import router
+from app.routes import router
 from utils import check_permissions
 from config import UPLOAD_FOLDER
 
